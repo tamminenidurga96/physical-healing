02dfa3d649b75e9d756005a79fd50c7015037c52
Your Name <your.email@example.com>
2025-03-19 11:29:25 +0530

    update footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaWhatsapp, FaPhone, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   const footerStyle = {
@@ -9,9 +10,43 @@ function Footer() {
     marginTop: "30px",
   };
 
+  const iconStyle = {
+    margin: "0 10px",
+    color: "white",
+    fontSize: "20px",
+  };
+
   return (
     <footer style={footerStyle}>
       <p>© 2025 DoctorCare. All rights reserved.</p>
+      
+      {/* Address */}
+      <p style={{ marginTop: "10px" }}>
+        📍 RTC COMPLEX BACKSIDE 3RD LINE, NEAR URBAN PRIMARY CENTER, SRIKAKULAM
+      </p>
+
+      {/* Contact Details */}
+      <p>
+        <FaWhatsapp style={iconStyle} /> +0000000000
+        <FaPhone style={iconStyle} /> +0000000000
+      </p>
+
+      {/* Instagram Link */}
+      <p>
+        <a
+          href="https://www.instagram.com/your_instagram_page"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "white", textDecoration: "none" }}
+        >
+          <FaInstagram style={iconStyle} /> Follow us on Instagram
+        </a>
+      </p>
+
+      {/* Explore Physiotherapy & Rehab */}
+      <p style={{ marginTop: "15px", fontWeight: "bold" }}>
+        Explore <span style={{ color: "#00BFFF" }}>PhysioRehab</span>
+      </p>
     </footer>
   );
 }
